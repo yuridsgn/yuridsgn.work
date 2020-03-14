08d2659a7817835058c9800e5cfb276717df9d72
Yuri <>
2020-03-14 19:26:32 +0800

    heroku setting

---
+++ index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="zh-tw" lang="zh-tw">
+  <head>
+    <meta http-equiv="refresh" content="0; url=/dist/index.html" />
+  </head>
+  <body></body>
+
+</html>
