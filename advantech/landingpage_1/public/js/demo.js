f5f04c47b77c1074db65196d634d3fe30f917d78
Yuri <>
2020-03-23 20:13:47 +0800

    加入影片輪播 成功案例展開

--- advantech/landingpage_1/public/js/demo.js
+++ advantech/landingpage_1/public/js/demo.js
@@ -17,15 +17,8 @@ $(document).ready(function () {
             prevEl: '.swiper-button-prev',
         },
 
-
-        // scrollbar
-        /*
-        scrollbar: {
-            el: '.swiper-scrollbar',
-        },
-        */
     })
-    
+    // 影片
         var lpVideo = new Swiper('.swiper-container.lp-video-container', {
         loop: true, // 循環模式
         autoplay: false, // 自動播放
@@ -41,73 +34,36 @@ $(document).ready(function () {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
-
-
-        // scrollbar
-        /*
-        scrollbar: {
-            el: '.swiper-scrollbar',
-        },
-        */
     })
 
-    var indexProductsSwiper = new Swiper('.swiper-container.index-products-swiper', {
+    // 產品
+    var lpProducts = new Swiper('.swiper-container.lp-product-container', {
 
         loop: true, // 循環模式
-        slidesPerView: 3,
-        spaceBetween: 0,
         pagination: {
             el: '.swiper-pagination',
             clickable: true,
         },
         // 切換前後按鈕
-        breakpoints: {
-            768: {
-                slidesPerView: 2.5,
-                spaceBetween: 10
-            }
-        },
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         }
 
-
     });
-  
-  
-    // 最新活動
-    var indexMainBanner = new Swiper('#newEventContainer', {
-        slidesPerView: 3,
-        // 分頁
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-
-        // 切換前後按鈕
-        breakpoints: {
-            767: {
-                slidesPerView: 1.2,
-                spaceBetween: 0
-            }
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-
-    })
 
 })
 
 
-//--Q&A收合圖示--
+//--Case展開--
 
   $(function() {
-    $(".faq-header").click(function() {
-      $(this).toggleClass("faq-header-bg-minus"); 
+    
+    $("#lp-case-more").click(function() {
+      $(this).hide(); 
+      $("#hide-case").fadeIn(600);
     });
+  
   });
   
 
